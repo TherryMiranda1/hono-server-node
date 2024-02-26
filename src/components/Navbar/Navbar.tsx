6374fef572e7831b5eb8ebe272376546ff540f08
laptop haru <therrymiranda1@gmail.com>
2024-02-26 19:00:28 +0000

    remove unnecesary ts files

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,3 @@
-import { LiaAddressCardSolid } from "react-icons/lia";
-import { PiCardsFill } from "react-icons/pi";
-import { BiHomeAlt, BiMessageDetail } from "react-icons/bi";
 import { links } from "../../paths/paths";
 
 
